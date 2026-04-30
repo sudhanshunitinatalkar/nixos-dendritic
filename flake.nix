f8b2619de6ff4e8f1c55992c98147d85c217394d
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-05-01 00:11:21 +0530

    update

--- flake.nix
+++ flake.nix
@@ -30,10 +30,6 @@
       url = "github:youwen5/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    hermes-agent = {
-      url = "github:NousResearch/hermes-agent"; 
-      inputs.nixpkgs.follows = "nixpkgs"; 
-    };
   };
     outputs =
     inputs:
